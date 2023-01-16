1b004ad5dabde29d9c5175d3874cf39d90cf7ab5
Denys <speleoden@gmail.com>
2023-01-16 18:21:57 +0100

    instr_par, roles + fixes

--- mmoda_tab_generator/tab_generator.py
+++ mmoda_tab_generator/tab_generator.py
@@ -34,8 +34,12 @@ class MMODATabGenerator:
                 item = json.loads(_item)
             
             if isinstance(item, list):
-                if item[0]['query_name'] in ('src_query', 'instr_query'):
+                if item[0]['query_name'] == 'src_query':
                     continue
+                elif item[0]['query_name'] == 'instr_query':
+                    for par in item[1:]:
+                        param_dict[par['name']] = par
+                        param_dict[par['name']]['in_instr_query'] = True
                 else:
                     product_name = item[1]['product_name']
                     products_list.append(product_name)
@@ -45,36 +49,39 @@ class MMODATabGenerator:
                         else:
                             param_dict[par['name']] = par
                             param_dict[par['name']]['products'] = [product_name]
+                        param_dict[par['name']]['in_instr_query'] = False
 
         return param_dict, products_list
 
 
-    def generate(self, instrument_name, instruments_dir_path):
+    def generate(self, instrument_name, instruments_dir_path, frontend_name, roles):
         param_dict, products_list = self._arrange_data(instrument_name)
         
-        this_instr_path = os.path.join(instruments_dir_path, f"mmoda_{instrument_name}")
+        this_instr_path = os.path.join(instruments_dir_path, f"mmoda_{frontend_name}")
         os.makedirs(this_instr_path, exist_ok=True)
-        basename = os.path.join(this_instr_path, f"mmoda_{instrument_name}")
+        basename = os.path.join(this_instr_path, f"mmoda_{frontend_name}")
         
         jenv = Environment(loader=PackageLoader('mmoda_tab_generator')) 
 
         templ = jenv.get_template('instr.info')
         with open(f"{basename}.info", 'w') as fd:
-            fd.write(templ.render(instrument_name = instrument_name))
+            fd.write(templ.render(instrument_name = frontend_name))
 
         templ = jenv.get_template('instr.module')
         with open(f"{basename}.module", 'w') as fd:
-            fd.write(templ.render(instrument_name = instrument_name))
+            fd.write(templ.render(instrument_name = frontend_name))
 
         templ = jenv.get_template('instr.install')
         with open(f"{basename}.install", 'w') as fd:
-            fd.write(templ.render(instrument_name = instrument_name,
+            fd.write(templ.render(instrument_name = frontend_name,
                                   defaults = [(k, v['value']) for k, v in param_dict.items()], 
-                                  default_product_type = products_list[0]))
+                                  default_product_type = products_list[0],
+                                  roles = roles))
 
         templ = jenv.get_template('instr.inc')
         with open(f"{basename}.inc", 'w') as fd:
-            fd.write(templ.render(instrument_name = instrument_name,
+            fd.write(templ.render(instrument_name = frontend_name,
+                                  dispatcher_name = instrument_name,
                                   param_dict = param_dict,
                                   products_list = products_list))
         
@@ -86,6 +93,8 @@ def main():
     parser.add_argument('-n', '--name', required=True)
     parser.add_argument('-p', '--path')
     parser.add_argument('-c', '--config')
+    parser.add_argument('-r', '--roles', default='developer')
+    parser.add_argument('--frontend_name')
     args = parser.parse_args()
     
     if not args.config:
@@ -98,9 +107,11 @@ def main():
     dispatcher_url = args.url if args.url else conf.dispatcher_url
     instruments_dir_path = args.path if args.path else conf.instruments_dir_path
     instrument_name = args.name
-
+    frontend_name = args.frontend_name if args.frontend_name else instrument_name
+    roles = args.roles
+    
     generator = MMODATabGenerator(dispatcher_url)
-    generator.generate(instrument_name, instruments_dir_path)
+    generator.generate(instrument_name, instruments_dir_path, frontend_name, roles)
     
 if __name__ == '__main__':
     main()
