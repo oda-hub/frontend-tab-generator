4254c1a5dfa4e254aec3add854fa23b0be14ef43
burnout87 <gabriele.barni@gmail.com>
2024-09-24 11:09:18 +0200

    check hdu is null

--- mmoda_tab_generator/templates/euclid/mmoda_euclid.js
+++ mmoda_tab_generator/templates/euclid/mmoda_euclid.js
@@ -45,44 +45,48 @@
         //Position of the header and data unit
         let hdu_index = 1;
 	    let hdu = fits_file.getHDU(hdu_index);
-	    let data = hdu.data;
+        if(hdu != null) {
+            let data = hdu.data;
 
-        let selector_container = document.querySelectorAll('.euclid-instruments-filters.multivalued-field');
+            let selector_container = document.querySelectorAll('.euclid-instruments-filters.multivalued-field');
 
-        selector_container[0].addEventListener('change', function(event) {
-            let selector_filter = event.target;
-            if (selector_filter.parentElement.classList.contains("instrument-filters")) {
-                let selected_filter = event.target.value.toLowerCase();
-                let divBlock = selector_filter.closest('div.row.form-group.multivalued-value');
-                let selector_filter_flux = divBlock.querySelector('[id$="flux-"]');
-                let selector_filter_flux_error = divBlock.querySelector('[id$="flux-error-"]');
+            selector_container[0].addEventListener('change', function(event) {
+                let selector_filter = event.target;
+                if (selector_filter.parentElement.classList.contains("instrument-filters")) {
+                    let selected_filter = event.target.value.toLowerCase();
+                    let divBlock = selector_filter.closest('div.row.form-group.multivalued-value');
+                    let selector_filter_flux = divBlock.querySelector('[id$="flux-"]');
+                    let selector_filter_flux_error = divBlock.querySelector('[id$="flux-error-"]');
 
-                f0 = selected_filter.split('|')[0]
-                f1 = selected_filter.split('|')[1]
-                f2 = f1.split('.')[1]
-                f1 = f1.split('.')[0]
+                    f0 = selected_filter.split('|')[0]
+                    f1 = selected_filter.split('|')[1]
+                    f2 = f1.split('.')[1]
+                    f1 = f1.split('.')[0]
 
-                let adapted_str_flux = str_flux.slice();
-                adapted_str_flux.push('_' + f2, f2 + '_', '.' + f2, f2 + '.', f0, f1, 'mag_' + f1, 'f' + f1);
-                adapted_str_flux.unshift(f2);
+                    let adapted_str_flux = str_flux.slice();
+                    adapted_str_flux.push('_' + f2, f2 + '_', '.' + f2, f2 + '.', f0, f1, 'mag_' + f1, 'f' + f1);
+                    adapted_str_flux.unshift(f2);
 
-                let adapted_str_err = str_err.slice();
-                adapted_str_err.push('_' + f2, f2 + '_', '.' + f2, f2 + '.', f0, f1, 'emag_' + f1, 'e' + f1, 'ef' + f1);
-                adapted_str_err.unshift(f2);
+                    let adapted_str_err = str_err.slice();
+                    adapted_str_err.push('_' + f2, f2 + '_', '.' + f2, f2 + '.', f0, f1, 'emag_' + f1, 'e' + f1, 'ef' + f1);
+                    adapted_str_err.unshift(f2);
 
-                let list_columns_names_ordered = guess_columns(data.columns, adapted_str_flux);
-                let list_columns_errors_ordered = guess_columns(data.columns, adapted_str_err);
+                    let list_columns_names_ordered = guess_columns(data.columns, adapted_str_flux);
+                    let list_columns_errors_ordered = guess_columns(data.columns, adapted_str_err);
 
-                selector_filter_flux.innerHTML = [{value: '', text: '- Select -'}].concat(list_columns_names_ordered.map(column => ({value: column, text: column}))).map(option => `<option value="${option.value}"${option.text === '- Select -' ? ' selected="selected"' : ''}>${option.text}</option>`).join('');
-                selector_filter_flux_error.innerHTML = [{value: '', text: '- Select -'}].concat(list_columns_errors_ordered.map(column => ({value: column, text: column}))).map(option => `<option value="${option.value}"${option.text === '- Select -' ? ' selected="selected"' : ''}>${option.text}</option>`).join('');
-            }
-        });
+                    selector_filter_flux.innerHTML = [{value: '', text: '- Select -'}].concat(list_columns_names_ordered.map(column => ({value: column, text: column}))).map(option => `<option value="${option.value}"${option.text === '- Select -' ? ' selected="selected"' : ''}>${option.text}</option>`).join('');
+                    selector_filter_flux_error.innerHTML = [{value: '', text: '- Select -'}].concat(list_columns_errors_ordered.map(column => ({value: column, text: column}))).map(option => `<option value="${option.value}"${option.text === '- Select -' ? ' selected="selected"' : ''}>${option.text}</option>`).join('');
+                }
+            });
 
-        updateSelectorList("mmoda_photoz_euclid_column_name_MW_EBV", keys_MW_EBV, data.columns);
-        updateSelectorList("mmoda_photoz_euclid_column_name_RA", keys_RA, data.columns);
-        updateSelectorList("mmoda_photoz_euclid_column_name_DEC", keys_DEC, data.columns);
-        updateSelectorList("mmoda_photoz_euclid_column_name_Ztrue", keys_Ztrue, data.columns);
-        updateSelectorList("mmoda_photoz_euclid_column_name_Nz_prior_I", keys_NZ_Prior_I, data.columns);
+            updateSelectorList("mmoda_photoz_euclid_column_name_MW_EBV", keys_MW_EBV, data.columns);
+            updateSelectorList("mmoda_photoz_euclid_column_name_RA", keys_RA, data.columns);
+            updateSelectorList("mmoda_photoz_euclid_column_name_DEC", keys_DEC, data.columns);
+            updateSelectorList("mmoda_photoz_euclid_column_name_Ztrue", keys_Ztrue, data.columns);
+            updateSelectorList("mmoda_photoz_euclid_column_name_Nz_prior_I", keys_NZ_Prior_I, data.columns);
+        }
+        else
+            console.error(`Error: HDU not found at the index ${hdu_index}`);
 
     }
 
